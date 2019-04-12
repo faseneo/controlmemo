73b66a07daec168b4aa45d07c5e56a994568f538
fsegovia <fsegovian@gmail.com>
2019-04-12 15:14:10 -0400

    Cambios en el header

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <?php header("Access-Control-Allow-Origin: *");
 ?>
        <!-- <meta charset="utf-8">-->
-		<meta http-equiv="Content-type" content="text/html; charset=UTF-8/>
+		<meta http-equiv="Content-type" content="text/html; charset=UTF-8/">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- Los 3 meta tags anteriores *deben* ser lo primeros en el head; caulquier otro contenido en el head debe venir *despues* de estos tags -->
         <meta name="description" content="Control Adquisiciones">
@@ -9,9 +9,11 @@
         <link href="img/templates/favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon">
  
         <title>Control Adquisiciones Umce</title>
-        <!-- Bootstrap core CSS -->
-        <link rel="stylesheet" href="bootstrap/3.3.7/css/bootstrap.css" type="text/css">
-        <!-- <link rel="stylesheet" href="http://static.umce.cl/bootstrap/3.3.7/css/bootstrap.css" type="text/css"> -->
+        <link rel="stylesheet" href="bootstrap/3.3.7/css/bootstrap.min.css" type="text/css">
+        
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+        <!-- Bootstrap core CSS 
+             <link rel="stylesheet" href="http://static.umce.cl/bootstrap/3.3.7/css/bootstrap.css" type="text/css"> -->
         <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"> -->
 
         <link href="css/estilocontrolmemo.css" rel="stylesheet" type="text/css">
@@ -24,9 +26,8 @@
         ================================================== -->
         <!-- Placed at the end of the document so the pages load faster -->
         <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> -->
-        <script src="js/jquery/1.12.4/jquery.min.js"></script>
         <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script> -->
-
+        <script src="js/jquery/1.12.4/jquery.min.js"></script>
         <!-- <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery.min.js"><\/script>')</script> -->
         <!-- <script src="js/bootstrap.min.js"></script> -->
 
@@ -36,7 +37,6 @@
         <script src="bootstrap/3.3.7/js/bootstrap.js"></script>
 
         <!-- <script src="bootstrap-datepicker/js/bootstrap-datetimepicker.js"></script> -->
-
         <!-- <script src="bootstrap-datepicker-1.6.4-dist/js/bootstrap-datepicker.min.js"></script>
         <script src="bootstrap-datepicker-1.6.4-dist/locales/bootstrap-datepicker.en-GB.min.js" charset="UTF-8"></script>
         <script src="bootstrap-datepicker-1.6.4-dist/locales/bootstrap-datepicker.es.min.js" charset="UTF-8"></script>
