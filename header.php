b4b22934ec5b00649bf20e729ff8ad4234e1dd78
fsegovia <fsegovian@gmail.com>
2019-04-12 15:21:12 -0400

    cambio de posicion del css

--- header.php
+++ header.php
@@ -16,13 +16,13 @@
              <link rel="stylesheet" href="http://static.umce.cl/bootstrap/3.3.7/css/bootstrap.css" type="text/css"> -->
         <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"> -->
 
-        <link href="css/estilocontrolmemo.css" rel="stylesheet" type="text/css">
-        
         <!-- <link id="bsdp-css" href="bootstrap-datepicker-1.6.4-dist/css/bootstrap-datepicker3.min.css" rel="stylesheet"> -->
         <!-- <link href="bootstrap-datepicker/css/bootstrap-datetimepicker.css" rel="stylesheet"> -->
         <!-- <link href="bootstrap-datepicker/css/font-awesome.min.css" rel="stylesheet"> -->
-
-         <!-- Bootstrap core JavaScript
+        
+        <!--  css app siempre al final de los demas css -->
+        <link href="css/estilocontrolmemo.css" rel="stylesheet" type="text/css">
+        <!-- Bootstrap core JavaScript
         ================================================== -->
         <!-- Placed at the end of the document so the pages load faster -->
         <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> -->
