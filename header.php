7e33cc02d9e12e1a2c0623cfe74b72acd0b073ca
fsegovia <fsegovian@gmail.com>
2019-05-10 14:23:04 -0400

    cargo nueva version bootstrap 3.4.1

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
  
         <title>SICAD Umce</title>
 
-        <link rel="stylesheet" href="bootstrap/3.3.7/css/bootstrap.min.css" type="text/css">
+        <link rel="stylesheet" href="bootstrap/3.4.1/css/bootstrap.min.css" type="text/css">
         <!-- Bootstrap core CSS 
              <link rel="stylesheet" href="http://static.umce.cl/bootstrap/3.3.7/css/bootstrap.css" type="text/css"> -->
         <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"> -->
@@ -34,7 +34,7 @@
         <!-- <script src="bootstrap-datepicker/js/moment-with-locales.js"></script> -->
         <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script> -->
 
-        <script src="bootstrap/3.3.7/js/bootstrap.js"></script>
+        <script src="bootstrap/3.4.1/js/bootstrap.js"></script>
 
 
         <!-- <script src="bootstrap-datepicker/js/bootstrap-datetimepicker.js"></script> -->
